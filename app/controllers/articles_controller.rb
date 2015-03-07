7927474b06e1751a4f97f036763366d5d5ea5f19
momo9 <liangchenchi@gmail.com>
2015-03-08 03:51:58 +0800

    1. create (model) 2. show 3. destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,31 @@
 class ArticlesController < ApplicationController
-  def new
-  end
+    def new
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find params[:id]
+    end
+
+    def create
+        @article = Article.new articleParams
+
+        @article.save
+        redirect_to @article
+    end
+
+    def destroy
+        @article = Article.find params[:id]
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
+    private
+    def articleParams
+        params.require(:article).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'articles/new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
